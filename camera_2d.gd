e9c073b6c902406c3458b59b585c5d4785d18039
Jahn815 <pairtoll@gmail.com>
2026-01-15 15:37:22 -0500

    camera limit and directional correction

--- camera_2d.gd
+++ camera_2d.gd
@@ -8,17 +8,21 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	#position.x += 1
+	#var local_mouse_pos = get_local_mouse_position()
+	#if local_mouse_pos.x>570: 
+	#	position.x-=1
+	#if local_mouse_pos.x<-570: 
+	#	position.x+=1
+	#pass
+	
 	var local_mouse_pos = get_local_mouse_position()
-	if local_mouse_pos.x>570: 
-		position.x-=1
-	if local_mouse_pos.x<-570: 
-		position.x+=1
-	pass
-	#guys i got it to work :l
-	#another test just in case
-	#test123
+	#print(local_mouse_pos)
+	#print(position.x)
+	
+	if (local_mouse_pos.x > 400 and position.x < 250):
+		position.x+=8
+	if (local_mouse_pos.x < -400 and position.x > -250):
+		position.x-=8
 #<<<<<<< HEAD
-	#final test123 now save it
 #=======
-	#hi justin :D
 #>>>>>>> 5765b53839f9b6d78099af54445ffe8f346efdf7
