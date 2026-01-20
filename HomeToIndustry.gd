d204cb32b1301f565ec2d35a86de2840fa1bba36
williammo29 <william2011mo@gmail.com>
2026-01-20 13:16:44 -0500

    UI changes

--- HomeToIndustry.gd
+++ HomeToIndustry.gd
@@ -3,9 +3,7 @@ extends Control
 @onready var button_industry1: Button = $Industry1
 @onready var button_industry2: Button = $Industry2
 @onready var button_industry3: Button = $Industry3
-@onready var button_industry4: Button = $Industry4
-@onready var button_industry5: Button = $Industry5
-
+@onready var home_button: Button = $HomeButton
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -14,8 +12,17 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	button_industry1.pressed.connect(_gotoindustry1)
+	button_industry2.pressed.connect(_gotoindustry2)
+	button_industry3.pressed.connect(_gotoindustry3)
+	home_button.pressed.connect(_gotohome)
 	pass
 	
 	
 func _gotoindustry1(): #rename this to goto[industryname]
-	get_tree().change_scene_to_file("res://industriesandstocks.tscn")
+	get_tree().change_scene_to_file("res://Stocks1.tscn")
+func _gotoindustry2(): #rename this to goto[industryname]
+	get_tree().change_scene_to_file("res://Stocks2.tscn")
+func _gotoindustry3(): #rename this to goto[industryname]
+	get_tree().change_scene_to_file("res://Stocks3.tscn")
+func _gotohome(): 
+	get_tree().change_scene_to_file("res://test2.tscn")
