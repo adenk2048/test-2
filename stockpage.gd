8471e0d3cb8edce01d02cdfcf6a12d302cd7eb7f
williammo29 <william2011mo@gmail.com>
2026-01-27 10:41:36 -0500

    Added a working display for money on the stock page and fixed some bugs that would break the game (still need to do more)

--- stockpage.gd
+++ stockpage.gd
@@ -7,6 +7,7 @@ extends Control
 @onready var buy_button: Button = $Buy
 @onready var sell_button: Button = $Sell
 @onready var stock_input: LineEdit = $NumberOfStocks
+@onready var money_value: Label = $Money
 var currentStock = 0;
 var stockCost = 10;
 
@@ -17,6 +18,7 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	money_value.text = "$" + str(MoneyAndStocks.money)
 	home_button.pressed.connect(_gotohome)
 	industry1_button.pressed.connect(_gotoindustry1)
 	industry2_button.pressed.connect(_gotoindustry2)
@@ -38,12 +40,14 @@ func _buy():
 	print(stock_input.text)
 	print("bought " + stock_input.text + " stocks")
 	MoneyAndStocks.stocks[currentStock]+=int(stock_input.text)
-	MoneyAndStocks.money-=stockCost
+	if !stock_input.text == "":
+		MoneyAndStocks.money-=stockCost*int(stock_input.text)
 	print("You now have " + str(MoneyAndStocks.stocks[currentStock]) + " stocks")
 func _sell():
 	print(stock_input.text)
 	print("sold " + stock_input.text + " stocks")
 	MoneyAndStocks.stocks[currentStock]-=int(stock_input.text)
-	MoneyAndStocks.money+=stockCost
+	if !stock_input.text == "":
+		MoneyAndStocks.money+=stockCost*int(stock_input.text)
 	print("You now have " + str(MoneyAndStocks.stocks[currentStock]) + " stocks")
 	
