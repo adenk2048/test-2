c7352ad8f4e882350d7a63241df22dc19610db7f
Jahn815 <pairtoll@gmail.com>
2026-02-04 13:24:42 -0500

    Added shop, 3 items, ability to buy the items, and also money limitations
    a

--- shop.gd
+++ shop.gd
@@ -1,5 +1,12 @@
 extends Control
 @onready var sugar_button: Button = $BuySugar
+@onready var coffee_button: Button = $BuyCoffee
+@onready var fifty_button: Button = $BuyFifty
+@onready var back_button: Button = $BackButton
+
+var sugar = 0
+var coffee = 0
+var fiftyhourenergy = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -8,4 +15,32 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	sugar_button.pressed.connect(_buysugar)
+	coffee_button.pressed.connect(_buycoffee)
+	fifty_button.pressed.connect(_buyfifty)
+	back_button.pressed.connect(_back)
 	pass
+	
+func _buysugar():
+	if (MoneyAndStocks.money - 5 < 0):
+		print("You're broke bruh")
+	else:
+		print("bought sugar")
+		MoneyAndStocks.money -= 5
+
+func _buycoffee():
+	if (MoneyAndStocks.money - 10 < 0):
+		print("You're broke bruh")
+	else:
+		print("bought coffee")
+		MoneyAndStocks.money -= 10
+
+func _buyfifty():
+	if (MoneyAndStocks.money - 50 < 0):
+		print("You're broke bruh")
+	else:
+		print("bought 50 hour energy")
+		MoneyAndStocks.money -= 50
+
+func _back():
+	get_tree().change_scene_to_file("res://robin_good_home.tscn")
